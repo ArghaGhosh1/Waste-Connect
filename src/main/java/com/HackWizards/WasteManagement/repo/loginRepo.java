cc8d6092220f02c133c1f852c64b4e25b31a80b5
ArghaGhosh1 <arghaghosh276@hotmail.com>
2025-08-05 20:38:50 +0530

    hi

---
+++ src/main/java/com/HackWizards/WasteManagement/repo/loginRepo.java
@@ -0,0 +1,11 @@
+//package com.HackWizards.WasteManagement.repo;
+//
+//import com.HackWizards.WasteManagement.model.Ragister;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+//@Repository
+//public interface loginRepo extends JpaRepository<Ragister, String>
+//{
+//    Ragister findByUsername(String username);
+//}
