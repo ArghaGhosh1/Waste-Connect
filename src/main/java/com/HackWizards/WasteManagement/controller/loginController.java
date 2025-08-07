1f915aa33955ddedd0d21ee643f2540946712738
ArghaGhosh1 <arghaghosh276@hotmail.com>
2025-08-07 22:41:57 +0530

    hi

---
+++ src/main/java/com/HackWizards/WasteManagement/controller/loginController.java
@@ -0,0 +1,17 @@
+//package com.HackWizards.WasteManagement.controller;
+//
+//import jakarta.servlet.http.HttpServletRequest;
+//import org.springframework.web.bind.annotation.CrossOrigin;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@CrossOrigin
+//public class loginController
+//{
+//    @GetMapping("/")
+//    public String greet(HttpServletRequest request)
+//    {
+//        return "welcome to my website"+ request.getSession().getId();
+//    }
+//}
