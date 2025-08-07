dd6c9da50536b7592d61585b4dabef1f008302a3
ArghaGhosh1 <arghaghosh276@hotmail.com>
2025-08-07 22:43:03 +0530

    hi

---
+++ src/test/java/com/HackWizards/WasteManagement/WasteManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HackWizards.WasteManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WasteManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
