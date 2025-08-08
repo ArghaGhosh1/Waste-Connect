a6ab52464f0de29ed2ff580122970d43dbfb7bd0
ArghaGhosh1 <arghaghosh276@hotmail.com>
2025-08-08 23:07:27 +0530

    hi

---
+++ src/main/java/com/HackWizards/WasteManagement/WasteManagementApplication.java
@@ -0,0 +1,14 @@
+package com.HackWizards.WasteManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WasteManagementApplication {
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(WasteManagementApplication.class, args);
+	}
+
+}
