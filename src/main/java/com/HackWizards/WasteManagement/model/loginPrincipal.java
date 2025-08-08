5fbf15377b5aec6d4abb45d5df53e638e48e4782
ArghaGhosh1 <arghaghosh276@hotmail.com>
2025-08-08 23:06:34 +0530

    hi

---
+++ src/main/java/com/HackWizards/WasteManagement/model/loginPrincipal.java
@@ -0,0 +1,53 @@
+//package com.HackWizards.WasteManagement.model;
+//
+//import org.springframework.security.core.GrantedAuthority;
+//import org.springframework.security.core.authority.SimpleGrantedAuthority;
+//import org.springframework.security.core.userdetails.UserDetails;
+//
+//import java.util.Collection;
+//import java.util.Collections;
+//
+//public class loginPrincipal implements UserDetails
+//{
+//    private Ragister user;
+//
+//    public loginPrincipal(Ragister user)
+//    {
+//        this.user = user;
+//    }
+//
+//    @Override
+//    public Collection<? extends GrantedAuthority> getAuthorities() {
+//        return Collections.singleton(new SimpleGrantedAuthority("USER"));
+//    }
+//
+//    @Override
+//    public String getPassword() {
+//        return user.getPassword();
+//    }
+//
+//    @Override
+//    public String getUsername() {
+//        return user.getUsername();
+//    }
+//
+//    @Override
+//    public boolean isAccountNonExpired() {
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean isAccountNonLocked() {
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean isCredentialsNonExpired() {
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean isEnabled() {
+//        return true;
+//    }
+//}
